1f4e0938b1160f0858344ec83acf705cd2503d07
DarioConiglione <dariocng13@gmail.com>
2025-10-07 15:39:23 +0100

    Insert "fizzbuzz" instead of both multiples

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,5 +27,9 @@ for (let number = 1; number <= 100; number++) {
         console.log(number)
     }
 
+    if ((number % 3 == 0) && (number % 5 == 0)) {
+        console.log("FizzBuzz")  
+    }
+
 }
 
